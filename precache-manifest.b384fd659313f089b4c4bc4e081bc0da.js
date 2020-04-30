e3b4b4462b6a5192a374b2b25edd5aa7741c8979
aditya singh <caditya1997@gmail.com>
2020-04-30 20:18:39 +0530

    please chal ja..

---
+++ precache-manifest.b384fd659313f089b4c4bc4e081bc0da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b45d0b8b6d59a579e372a75c08cd33a",
+    "url": "/http//bulletproofadks.github.io/layout/index.html"
+  },
+  {
+    "revision": "a2ae674ad7368c42a54b",
+    "url": "/http//bulletproofadks.github.io/layout/static/css/main.88118110.chunk.css"
+  },
+  {
+    "revision": "077da2870614acdb1631",
+    "url": "/http//bulletproofadks.github.io/layout/static/js/2.54cc66b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//bulletproofadks.github.io/layout/static/js/2.54cc66b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2ae674ad7368c42a54b",
+    "url": "/http//bulletproofadks.github.io/layout/static/js/main.02e7cda1.chunk.js"
+  },
+  {
+    "revision": "443a36cfe1ae42e07d7f",
+    "url": "/http//bulletproofadks.github.io/layout/static/js/runtime-main.ad5509ac.js"
+  }
+]);
